15cad59c460b15290c66edef25f09e8f06cd4b24
Miel maswary <73788959+mielmaswary@users.noreply.github.com>
2022-11-20 10:23:15 +0200

    fix links button

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,7 +13,7 @@ const Button = (props) => {
     setOrderRecived(true);
     setCart([]);
     setTimeout(() => {
-      navigate("/");
+      navigate("/picsome/");
       setOrderRecived(false);
     }, 3000);
   };
